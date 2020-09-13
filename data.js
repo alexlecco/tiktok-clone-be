8657e887cbaeedc195ccb235372caab11a043c04
alexlecco <alexlecco24@gmail.com>
2020-09-13 20:59:53 -0300

    create /v1/posts endpoint to serve data.js info

---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    url:
+      'https://v77.tiktokcdn.com/4bfd3debda2a6adae78af3001782a53d/5f5e43bf/video/tos/useast2a/tos-useast2a-ve-0068c004/31d62eb70a0a4adeb9eeefdd17f8a1b2/?a=1233\u0026br=4170\u0026bt=2085\u0026cr=0\u0026cs=0\u0026dr=0\u0026ds=3\u0026er=\u0026l=202009131007140101890720322C039690\u0026lr=tiktok_m\u0026mime_type=video_mp4\u0026qs=0\u0026rc=M2xoNHZucjhsdDMzNjczM0ApODU8OzY7Ojs4Nzo7ODY8O2dsM2hgMi5lZmNfLS0vMTZzczAzY2JeNGBfMV5gNDBhY2E6Yw%3D%3D\u0026vl=\u0026vr=',
+    channel: 'alexlecco',
+    description: 'This is some description',
+    song: 'Lu2vyk - Hold on',
+    likes: '111',
+    messages: '333',
+    shares: '222',
+  },
+  {
+    url:
+      'https://v16m.tiktokcdn.com/6ee1fdf5c225b63a28cc0e4efd1c449e/5f5e3fb1/video/tos/useast2a/tos-useast2a-ve-0068c003/4a3ec7b62604414799fd02bc85de3ee4/?a=1233\u0026br=2000\u0026bt=1000\u0026cr=0\u0026cs=0\u0026dr=0\u0026ds=3\u0026er=\u0026l=202009130949230101901861420B03736D\u0026lr=tiktok_m\u0026mime_type=video_mp4\u0026qs=0\u0026rc=MztwZ2lyNjRvdjMzZjczM0ApZmhoNDs6ZGU5Nzg5NGRmNWdxaF5mNGEuL2VfLS02MTZzc18vMmFiMi1gYS4tMjVeMjE6Yw%3D%3D\u0026vl=\u0026vr=',
+    channel: 'luisitocomunica',
+    description: 'viajando por Tailandia',
+    song: 'lu2vyk - Deep end',
+    likes: '11',
+    messages: '22',
+    shares: '33',
+  },
+];
+
+export default data;
